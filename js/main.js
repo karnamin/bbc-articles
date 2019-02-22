e88d3d0733d5bb1f1f634ed73200bfcb221cb616
karnamin <karnamin7@gmail.com>
2019-02-22 16:03:48 +0000

    Adds makeContent function

--- js/main.js
+++ js/main.js
@@ -23,7 +23,25 @@ window.onload = function () {
         })
         .catch(error => console.log(error))
 
-        function makeContent(data) {
+    function makeContent(data) {
+        mainBody.innerHTML = "";
+        title.textContent = data.title;
+        heading = `<h4>${data.body[0].model.text}</h4>`;
+        mainBody.innerHTML += heading;
 
+        for (var key in data.body) {
+            if (data.body[key].type === 'paragraph') {
+                para = `<p>${data.body[key].model.text}</p>`;
+                mainBody.innerHTML += para;
+            }
+            else if (data.body[key].type === 'image') {
+                console.log(data.body[key].model.url);
+                let img = new Image();
+                img.src = data.body[key].model.url;
+                mainBody.appendChild(img);
+            }
+            else if(data.body[key].type === 'list') {
+            }
         }
+    }
 }
