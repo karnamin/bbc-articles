5424b8fff2a8bf2e42b6c64ad859ebbd3d9da8a3
karnamin <karnamin7@gmail.com>
2019-02-22 18:47:06 +0000

    Adds previous article button and functionality

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ const allUrls = [`https://raw.githubusercontent.com/bbc/news-coding-test-dataset
     `https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-5.json`];
 
 let nextArticleButton = document.getElementById("next-article");
+const prevArticleButton = document.getElementById("prev-article");
+
 let pageCounter = 0;
 let heading;
 let para;
@@ -25,39 +27,50 @@ window.onload = function () {
         })
         .catch(error => console.log(error))
 
+    prevArticleButton.addEventListener("click", function () {
+        if (pageCounter === 0) {
+            alert('This is the first page, cannot go back');
+            return;
+        }
+        if (pageCounter < (allUrls.length - 1)) {
+            pageCounter--;
+            loadPage(pageCounter);
+        }
+    });
+
+    nextArticleButton.addEventListener("click", function () {
+        if (pageCounter === 3) {
+            pageCounter++;
+            loadPage(pageCounter);
+            nextArticleButton.innerHTML = "Go to ratings";
+            loadRatingPage();
+        }
+        if (pageCounter < (allUrls.length - 1)) {
+            pageCounter++;
+            loadPage(pageCounter);
+            console.log(pageCounter);
+        }
+    });
+
+    function loadRatingPage() {
         nextArticleButton.addEventListener("click", function () {
-            if (pageCounter === 3) {
-                pageCounter++;
-                loadPage(pageCounter);
-                nextArticleButton.innerHTML = "Go to ratings";
-                loadRatingPage();
-            }
-            if (pageCounter < (allUrls.length - 1)) {
-                pageCounter++;
-                loadPage(pageCounter);
-                console.log(pageCounter);
-            }
+            window.location.href = "./ratingsPage.html";
         });
-    
-        function loadRatingPage() {
-            nextArticleButton.addEventListener("click", function () {
-                window.location.href = "./ratingsPage.html";
-            });
-        }
-    
-        function loadPage(pageCounter) {
-            url = allUrls[pageCounter];
-            fetch(url)
-                .then(response => {
-                    return response.json();
-    
-                })
-                .then(data => {
-                    console.log(data);
-                    makeContent(data);
-                })
-                .catch(error => console.log(error))
-        }
+    }
+
+    function loadPage(pageCounter) {
+        url = allUrls[pageCounter];
+        fetch(url)
+            .then(response => {
+                return response.json();
+
+            })
+            .then(data => {
+                console.log(data);
+                makeContent(data);
+            })
+            .catch(error => console.log(error))
+    }
 
     function makeContent(data) {
         mainBody.innerHTML = "";
@@ -75,7 +88,7 @@ window.onload = function () {
                 height="${data.body[key].model.height}" width="${data.body[key].model.width}">`;
                 mainBody.innerHTML += image;
             }
-            else if(data.body[key].type === 'list') {
+            else if (data.body[key].type === 'list') {
                 let listItems = "";
                 for (var item = 0; item < data.body[key].model.items.length; item++) {
                     listItems += "<li>" + data.body[key].model.items[item] + "</li>"
